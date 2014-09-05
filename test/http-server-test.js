30f4d1e008b3d707a764eb06ad4c1ac2becd9e96
Paul Melnikow <paul.melnikow@bodylabs.com>
2014-09-04 21:03:02 -0400

    Get CORS support working with XHR preflight
    Without this patch, http-server returns 404 on the preflight request (method: 'OPTIONS'). You can see the problem by loading an XHR request in Firefox.

--- test/http-server-test.js
+++ test/http-server-test.js
@@ -114,5 +114,31 @@ vows.describe('http-server').addBatch({
         }
       }
     }
+  },
+  'When cors is enabled': {
+    topic: function () {
+      var server = httpServer.createServer({
+        root: root,
+        cors: true
+      });
+      server.listen(8082);
+      this.callback(null, server);
+    },
+    'and given OPTIONS request': {
+      topic: function () {
+        request({
+          method: 'OPTIONS',
+          uri: 'http://127.0.0.1:8082/',
+          headers: {
+            'Access-Control-Request-Method': 'GET',
+            Origin: 'http://example.com',
+            'Access-Control-Request-Headers': 'Foobar'
+          }
+        }, this.callback);
+      },
+      'status code should be 204': function (err, res, body) {
+        assert.equal(res.statusCode, 204);
+      }
+    }
   }
 }).export(module);
