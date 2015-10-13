0989c2c2761504b763b14ec73e97fd29d9114991
Budleigh Salterton <michal.czapracki@arkena.com>
2015-10-13 14:14:01 +0200

    remove console.log

--- test/http-server-test.js
+++ test/http-server-test.js
@@ -151,7 +151,6 @@ vows.describe('http-server').addBatch({
         assert.equal(res.statusCode, 204);
       },
       'response Access-Control-Allow-Headers should contain X-Test': function (err, res) {
-        console.dir(res.headers);
         assert.ok(res.headers['access-control-allow-headers'].split(/\s*,\s*/g).indexOf('X-Test') >= 0, 204);
       }
     }
