3878ac31b2889a167e0da4309617bf3672f28119
amoldavsky <assaf.moldavsky@gmail.com>
2018-06-23 15:30:32 -0400

    transformed plugin tests

--- test/http-saber-plugins-test.js
+++ test/http-saber-plugins-test.js
@@ -1,84 +1,112 @@
-var assert = require('assert'),
-    path = require('path'),
-    fs = require('fs'),
-    vows = require('vows'),
-    request = require('request'),
-    httpServer = require('../lib/http-saber'),
-    testplugin = require('./http-saber-test-plugin/plugin'),
-    testPluginSchema = require('./http-saber-test-plugin/package.json');
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+var vows = require('vows');
+var request = require('request');
+var httpSaber = require('../lib/http-saber');
+var chai = require('chai');
+var assert = require('assert');
+var expect = require('chai').expect;
+var should = require('chai').should();
+var testplugin = require('./http-saber-test-plugin/plugin');
+var testPluginSchema = require('./http-saber-test-plugin/package.json');
 
 var root = path.join(__dirname, 'fixtures', 'root');
 
-vows.describe('http-saber-plugins').addBatch({
-  'When http-saber starts': {
-    topic: function () {
-      var server = httpServer.createServer({
-        root: root
-      });
+describe( 'http-saber-plugins', function() {
 
-      var _this = this;
-      server.listen({ port: 8100 }, function () {
-        _this.callback(null, server);
-      });
-    },
-    'a plugin': {
-      topic: function (server) {
-        var _this = this;
-        this.callback(server, server.plugins);
-      },
-      'should be found and registered': function (server, plugins) {
-        var pluginName = null;
-        for (var key in testPluginSchema.extensions['http-saber']) {
-          pluginName = key;
-          break;
-        }
+    describe( 'When http-saber starts', function(done) {
 
-        assert.isTrue(!!plugins[pluginName]);
-      }
-    }
-  },
-  'When our test plugin is registered': {
-    topic: function () {
-      // tracks responses from our callbacks
-      var callbackResponses = {};
-      callbackResponses[httpServer.EVENTS.INIT] = null;
-      callbackResponses[httpServer.EVENTS.INIT_DONE] = null;
-      callbackResponses[httpServer.EVENTS.REQUEST_RECEIVED] = null;
-
-      testplugin.onInitCallback = function (options, unionOptions) {
-        callbackResponses[httpServer.EVENTS.INIT] = true;
-      };
-      testplugin.onInitDoneCallback = function (options, unionOptions) {
-        callbackResponses[httpServer.EVENTS.INIT_DONE] = true;
-      };
-      testplugin.onRequestCallback = function (req, res) {
-        callbackResponses[httpServer.EVENTS.REQUEST_RECEIVED] = true;
-      };
-
-      this.callback(null, callbackResponses);
-    },
-    'a callback should be called': {
-      topic: function (pluginCBResponses) {
-        var server = httpServer.createServer({
-          root: root
+        var server = null;
+        before( function() {
+            server = httpSaber.createServer({
+                root: root
+            });
+            return new Promise( function( resolve, reject ) {
+                server.listen(8080, resolve);
+            });
         });
 
-        var _this = this;
-        server.listen({ port: 8101 }, function () {
-          _this.callback(null, pluginCBResponses);
+        beforeEach(function() {});
+        afterEach(function() {});
+
+        after(function() {
+            server.close();
         });
-      },
-      'on init': function (responses) {
-        assert.isTrue(responses[httpServer.EVENTS.INIT]);
-      },
-      'on init_done': function (responses) {
-        assert.isTrue(responses[httpServer.EVENTS.INIT_DONE]);
-      },
-      'on request': function (responses) {
-        request('http://127.0.0.1:8101/', function () {
-          assert.isTrue(responses[httpServer.EVENTS.REQUEST_RECEIVED]);
+
+        it('a plugin should be found', function(done) {
+            var pluginName = null;
+            for (var key in testPluginSchema.extensions['http-saber']) {
+                pluginName = key;
+                break;
+            }
+
+            assert.isTrue( !!server.plugins[ pluginName ] );
+            done();
         });
-      }
-    }
-  }
-}).export(module);
+
+    });
+    
+    describe( 'When our test plugin is registered', function(done) {
+
+        // tracks responses from our callbacks
+        var callbackResponses = {};
+        callbackResponses[httpSaber.EVENTS.INIT] = null;
+        callbackResponses[httpSaber.EVENTS.INIT_DONE] = null;
+        callbackResponses[httpSaber.EVENTS.REQUEST_RECEIVED] = null;
+      
+        var server = null;
+        before( function() {
+            testplugin.onInitCallback = function (options, unionOptions) {
+                callbackResponses[httpSaber.EVENTS.INIT] = true;
+            };
+            testplugin.onInitDoneCallback = function (options, unionOptions) {
+                callbackResponses[httpSaber.EVENTS.INIT_DONE] = true;
+            };
+            testplugin.onRequestCallback = function (req, res) {
+                callbackResponses[httpSaber.EVENTS.REQUEST_RECEIVED] = true;
+            };
+            server = httpSaber.createServer({
+                root: root
+            });
+            return new Promise( function( resolve, reject ) {
+                server.listen(8080, resolve);
+            });
+        });
+
+        beforeEach(function() {});
+        afterEach(function() {});
+
+        after(function() {
+            server.close();
+        });
+
+        it('a callback should be called', function(done) {
+            request('http://127.0.0.1:8080/file', function (err, res, body) {
+                assert.equal( res.statusCode, 200, 'status code should be 200' );
+
+                fs.readFile(path.join(root, 'file'), 'utf8', function (err, file) {
+                    assert.equal(body.trim(), file.trim(), 'file content should match content of served file');
+                    done();
+                });
+            });
+        });
+
+        it('on init', function (done) {
+            assert.isTrue(callbackResponses[httpSaber.EVENTS.INIT]);
+            done();
+        });
+        it('on init_done', function (done) {
+            assert.isTrue(callbackResponses[httpSaber.EVENTS.INIT_DONE]);
+            done();
+        });
+        it('on request', function (done) {
+            request('http://127.0.0.1:8080/', function () {
+                assert.isTrue(callbackResponses[httpSaber.EVENTS.REQUEST_RECEIVED]);
+                done();
+            });
+        });
+
+    });
+
+});
