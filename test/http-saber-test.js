a1d4605c9810ec55d95249b5db10200be0bdf843
amoldavsky <assaf.moldavsky@gmail.com>
2018-06-23 15:14:44 -0400

    finished main tests

--- test/http-saber-test.js
+++ test/http-saber-test.js
@@ -15,17 +15,20 @@ describe( 'http-saber', function() {
 
     describe( 'When http-saber is listening on 8080', function(done) {
 
-        var server = httpSaber.createServer({
-            root: root
-        });
-
-        beforeEach(function() {
+        var server = null;
+        before( function() {
+           server = httpSaber.createServer({
+               root: root
+           });
             return new Promise( function( resolve, reject ) {
                 server.listen(8080, resolve);
             });
         });
 
-        afterEach(function() {
+        beforeEach(function() {});
+        afterEach(function() {});
+
+        after(function() {
             server.close();
         });
 
@@ -68,22 +71,26 @@ describe( 'http-saber', function() {
     // TODO: move to robots plugin
     describe( 'When robots options is activated', function(done) {
 
-        var server = httpSaber.createServer({
-            root: root,
-            robots: true,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': 'true'
-            }
+        var server = null;
+        before( function() {
+            server = httpSaber.createServer({
+                root: root,
+                robots: true,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true'
+                }
+            });
+
+            return new Promise( function( resolve, reject ) {
+                server.listen(8080, resolve);
+            });
         });
 
-        beforeEach(function() {
-           return new Promise( function( resolve, reject ) {
-               server.listen(8080, resolve);
-           });
-        });
+        beforeEach(function() {});
+        afterEach(function() {});
 
-        afterEach(function() {
+        after(function() {
             server.close();
         });
 
@@ -104,150 +111,195 @@ describe( 'http-saber', function() {
 
     });
 
+    // TODO: move to proxy plugin?
+    describe( 'When http-saber is proxying from 8081 to 8080', function(done) {
+
+        var server = null;
+        var proxy = null;
+        before( function() {
+            server = httpSaber.createServer({
+                root: root
+            });
+            proxy = httpSaber.createServer({
+                root: path.join(__dirname, 'fixtures'),
+                proxy: 'http://127.0.0.1:8080/'
+            });
+
+            return Promise.all([
+                new Promise( function( resolve, reject ) {
+                    server.listen(8080, resolve);
+                }),
+                new Promise( function( resolve, reject ) {
+                    proxy.listen(8081, resolve);
+                })
+            ]);
+        });
+
+        beforeEach(function() {});
+        afterEach(function() {});
+
+        after(function() {
+            server.close();
+            proxy.close();
+        });
+
+        it('it should serve files from the proxy server root directory', function(done) {
+            request('http://127.0.0.1:8081/root/file', function (err, res, body) {
+                assert.equal(res.statusCode, 200, 'status code should be the endpoint code 200');
+                done();
+            });
+        });
+
+        it('file content should match content of served file', function(done) {
+            request('http://127.0.0.1:8081/root/file', function (err, res, body) {
+                assert.equal( res.statusCode, 200, 'status code should be 200' );
+
+                fs.readFile(path.join(root, 'file'), 'utf8', function (err, file) {
+                    assert.equal(body.trim(), file.trim(), 'file content should match content of served file');
+                    done();
+                });
+            });
+        });
+
+        it('it should fallback to the proxied server', function(done) {
+            request('http://127.0.0.1:8081/file', function (err, res, body) {
+                assert.equal(res.statusCode, 200, 'status code should be the endpoint code 200');
+
+                fs.readFile(path.join(root, 'file'), 'utf8', function (err, file) {
+                    assert.equal(body.trim(), file.trim(), 'file content should match content of served file');
+                    done();
+                });
+            });
+        });
+
+    });
+
+    // TODO: move to cors plugin
+    describe( 'When cors is enabled', function(done) {
+
+        var TEST_HEADER = 'X-Test';
+        var server = null;
+        before( function() {
+            server = httpSaber.createServer({
+                root: root,
+                cors: true,
+                corsHeaders: TEST_HEADER
+            });
+            return new Promise( function( resolve, reject ) {
+                server.listen(8080, resolve);
+            });
+        });
+
+        beforeEach(function() {});
+        afterEach(function() {});
+
+        after(function() {
+            server.close();
+        });
+
+        describe('and given OPTIONS request', function(done) {
+
+            it('should respond with status code 204', function(done) {
+                request({
+                    method: 'OPTIONS',
+                    uri: 'http://127.0.0.1:8080/',
+                    headers: {
+                        'Access-Control-Request-Method': 'GET',
+                        'Origin': 'http://example.com',
+                        'Access-Control-Request-Headers': 'Foobar'
+                    }
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 204);
+                    assert.ok(res.headers['access-control-allow-headers'].split(/\s*,\s*/g).indexOf( TEST_HEADER ) >= 0, 204);
+                    done();
+                });
+            });
+
+            it('CORS headers should be present', function(done) {
+                request({
+                    method: 'OPTIONS',
+                    uri: 'http://127.0.0.1:8080/',
+                    headers: {
+                        'Access-Control-Request-Method': 'GET',
+                        'Origin': 'http://example.com',
+                        'Access-Control-Request-Headers': 'Foobar'
+                    }
+                }, function (err, res, body) {
+
+                    // FIXME: loop through all headers and compare request to response headers
+
+                    assert.ok(res.headers['access-control-allow-headers'].split(/\s*,\s*/g).indexOf( TEST_HEADER ) >= 0, 204);
+                    done();
+                });
+            });
+
+        });
+
+    });
+
+    // TODO: move to logger plugin
+    describe( 'When logger is given', function(done) {
+
+        function Logger() {
+            var _this = this;
+            this.log = function ( req, res, error ) {
+                _this.callbacks.forEach(function( cb ) {
+                    cb( req, res, error );
+                });
+            };
+            this.messages = [];
+            this.callbacks = [];
+        }
+        var logger = new Logger();
+        var server = null;
+
+        before(function() {
+            server = httpSaber.createServer({
+                root: root,
+                logFn: logger.log
+            });
+            return new Promise( function( resolve, reject ) {
+                server.listen(8080, resolve);
+            });
+        });
+
+        beforeEach(function() {});
+        afterEach(function() {});
+
+        after(function() {
+            server.close();
+        });
+
+        it('it should be called', function(done) {
+            var isCalled = false;
+            logger.callbacks.push(function() {
+                isCalled = true;
+                done();
+            });
+            request('http://127.0.0.1:8080/file', function (err, res, body) {
+                assert.equal(isCalled, true, 'logger was called');
+                logger.callbacks.pop();
+                // done();
+            });
+        });
+
+        it('on a bad request an error should be passed', function(done) {
+            var isError = false;
+            logger.callbacks.push(function (req, res, error) {
+                // this will be called twice
+                // once on request, and once on response
+                isError = !!error;
+            });
+            request({
+                method: 'DELETE',
+                uri: 'http://127.0.0.1:8080/foo'
+            }, function() {
+                assert.equal(isError, true, 'error');
+                logger.callbacks.pop();
+                done();
+            });
+        });
+
+    });
+
 });
-// var root = path.join(__dirname, 'fixtures', 'root');
-//
-// vows.describe('http-saber').addBatch({
-//     'When http-saber is proxying from 8081 to 8080': {
-//       topic: function () {
-//         var _this = this;
-//         var proxyServer = httpSaber.createServer({
-//           proxy: 'http://127.0.0.1:8080/',
-//           root: path.join(__dirname, 'fixtures')
-//         });
-//         proxyServer.listen(8081, function () {
-//           _this.callback(null, proxyServer);
-//         });
-//       },
-//       'it should serve files from the proxy server root directory': {
-//         topic: function () {
-//           request('http://127.0.0.1:8081/root/file', this.callback);
-//         },
-//         'status code should be the endpoint code 200': function (res) {
-//           assert.equal(res.statusCode, 200);
-//         },
-//         'and file content': {
-//           topic: function (res, body) {
-//             var self = this;
-//             fs.readFile(path.join(root, 'file'), 'utf8', function (err, data) {
-//               self.callback(err, data, body);
-//             });
-//           },
-//           'should match content of the served file': function (err, file, body) {
-//             assert.equal(body.trim(), file.trim());
-//           }
-//         }
-//       },
-//       'it should fallback to the proxied server': {
-//         topic: function () {
-//           request('http://127.0.0.1:8081/file', this.callback);
-//         },
-//         'status code should be the endpoint code 200': function (res) {
-//           assert.equal(res.statusCode, 200);
-//         },
-//         'and file content': {
-//           topic: function (res, body) {
-//             var self = this;
-//             fs.readFile(path.join(root, 'file'), 'utf8', function (err, data) {
-//               self.callback(err, data, body);
-//             });
-//           },
-//           'should match content of the proxied served file': function (err, file, body) {
-//             assert.equal(body.trim(), file.trim());
-//           }
-//         }
-//       }
-//     }
-//   },
-//   'When cors is enabled': {
-//     topic: function () {
-//       var _this = this;
-//       var server = httpSaber.createServer({
-//         root: root,
-//         cors: true,
-//         corsHeaders: 'X-Test'
-//       });
-//
-//       server.listen(8082, function () {
-//         _this.callback(null, server);
-//       });
-//     },
-//     'and given OPTIONS request': {
-//       topic: function () {
-//         request({
-//           method: 'OPTIONS',
-//           uri: 'http://127.0.0.1:8082/',
-//           headers: {
-//             'Access-Control-Request-Method': 'GET',
-//             Origin: 'http://example.com',
-//             'Access-Control-Request-Headers': 'Foobar'
-//           }
-//         }, this.callback);
-//       },
-//       'status code should be 204': function (err, res) {
-//         assert.equal(res.statusCode, 204);
-//       },
-//       'response Access-Control-Allow-Headers should contain X-Test': function (err, res) {
-//         assert.ok(res.headers['access-control-allow-headers'].split(/\s*,\s*/g).indexOf('X-Test') >= 0, 204);
-//       }
-//     }
-//   },
-//   'When logger is given': {
-//     topic: function () {
-//       function Logger() {
-//         var _this = this;
-//         this.log = function (/* arguments */) {
-//           _this.callbacks.forEach(function (cb) {
-//             cb.apply(this, arguments);
-//           });
-//         };
-//       }
-//       Logger.prototype.callbacks = [];
-//       var logger = new Logger();
-//       var server = httpSaber.createServer({
-//         root: root,
-//         logFn: logger.log
-//       });
-//       var _this = this;
-//       server.listen({ port: 8083 }, function () {
-//         _this.callback(null, server, logger);
-//       });
-//     },
-//     'for a well formed request': {
-//       topic: function (server, logger) {
-//         var loggerResp = { isCalled: false };
-//         logger.callbacks.push(function () {
-//           loggerResp.isCalled = true;
-//         });
-//         var _this = this;
-//         request({
-//           method: 'OPTIONS',
-//           uri: 'http://127.0.0.1:8083/'
-//         }, function () {
-//           _this.callback(null, loggerResp);
-//         });
-//       },
-//       'logger should be called': function (loggerResp) {
-//         assert.equal(loggerResp.isCalled, true);
-//       }
-//     },
-//     'for a bad request': {
-//       topic: function (server, logger) {
-//         var loggerResp = { err: false };
-//         logger.callbacks.push(function (req, res, err) {
-//           loggerResp.err = err;
-//         });
-//         var _this = this;
-//         request({
-//           method: 'BOO',
-//           uri: 'http://127.0.0.1:8083/'
-//         }, function () {
-//           _this.callback(null, loggerResp);
-//         });
-//       },
-//       'error should be logged': function (loggerResp) {
-//         assert.equal(!!loggerResp.err, true);
-//       }
-//     }
-//   }
-// }).export(module);
