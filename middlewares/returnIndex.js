770cdf38ef1e7ce6ffb0bd9c29815f12b439c4df
deju <deju.tu@gmail.com>
2018-10-11 10:41:05 +0800

    chore(middleware): rename webpack to returnIndex in middleware

--- middlewares/returnIndex.js
+++ middlewares/returnIndex.js
@@ -2,11 +2,11 @@
 var fs = require('fs'),
     path = require('path');
 
-function webpack (req, res) {
+function returnIndex (req, res) {
     if (req.url.search(/\./) === -1) {
         return res.end(fs.readFileSync(path.join(process.cwd(), 'index.html')));
     }
     res.emit('next');
 }
-module.exports = webpack;
+module.exports = returnIndex;
 
