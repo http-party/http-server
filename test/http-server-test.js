f9943afc651c5de15a03cc5399f4e06ddc5f0553
Danny Dalton <daltond2@hawkmail.newpaltz.edu>
2018-11-17 19:41:21 -0500

    Added some existance checking for SSL certificates; changed caching policy algorithm; fixed grammar in testing
    Signed-off-by: Danny Dalton <daltond2@hawkmail.newpaltz.edu>

--- test/http-server-test.js
+++ test/http-server-test.js
@@ -8,7 +8,7 @@ var assert = require('assert'),
 var root = path.join(__dirname, 'fixtures', 'root');
 
 vows.describe('http-server').addBatch({
-  'When http-server is listening on 8080': {
+  'When http-server is listening on 8080,\n': {
     topic: function () {
       var server = httpServer.createServer({
         root: root,
@@ -41,7 +41,7 @@ vows.describe('http-server').addBatch({
         }
       }
     },
-    'when requesting non-existent file': {
+    'and a non-existent file is requested...': {
       topic: function () {
         request('http://127.0.0.1:8080/404', this.callback);
       },
@@ -49,7 +49,7 @@ vows.describe('http-server').addBatch({
         assert.equal(res.statusCode, 404);
       }
     },
-    'when requesting /': {
+    'requesting /': {
       topic: function () {
         request('http://127.0.0.1:8080/', this.callback);
       },
@@ -59,15 +59,15 @@ vows.describe('http-server').addBatch({
         assert.include(body, '/canYouSeeMe');
       }
     },
-    'when robots options is activated': {
+    'when robots option is toggled on...': {
       topic: function () {
-        request('http://127.0.0.1:8080/', this.callback);
+        request('http://127.0.0.1:8080/robots.txt', this.callback);
       },
       'should respond with status code 200 to /robots.txt': function (res) {
         assert.equal(res.statusCode, 200);
       }
     },
-    'and options include custom set http-headers': {
+    'and options include custom set http-headers...': {
       topic: function () {
         request('http://127.0.0.1:8080/', this.callback);
       },
@@ -76,7 +76,7 @@ vows.describe('http-server').addBatch({
         assert.equal(res.headers['access-control-allow-credentials'], 'true');
       }
     },
-    'When http-server is proxying from 8081 to 8080': {
+    'and the server is set to proxy port 8081 to 8080, ': {
       topic: function () {
         var proxyServer = httpServer.createServer({
           proxy: 'http://127.0.0.1:8080/',
@@ -85,7 +85,7 @@ vows.describe('http-server').addBatch({
         proxyServer.listen(8081);
         this.callback(null, proxyServer);
       },
-      'it should serve files from the proxy server root directory': {
+      '\nit should serve files from the proxy\'s root': {
         topic: function () {
           request('http://127.0.0.1:8081/root/file', this.callback);
         },
@@ -104,7 +104,7 @@ vows.describe('http-server').addBatch({
           }
         }
       },
-      'it should fallback to the proxied server': {
+      '\nit should fallback to the proxied server': {
         topic: function () {
           request('http://127.0.0.1:8081/file', this.callback);
         },
@@ -125,7 +125,7 @@ vows.describe('http-server').addBatch({
       }
     }
   },
-  'When cors is enabled': {
+  'When CORS is enabled': {
     topic: function () {
       var server = httpServer.createServer({
         root: root,
@@ -135,7 +135,7 @@ vows.describe('http-server').addBatch({
       server.listen(8082);
       this.callback(null, server);
     },
-    'and given OPTIONS request': {
+    'and the server is given an OPTIONS request': {
       topic: function () {
         request({
           method: 'OPTIONS',
