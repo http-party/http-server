03a2c592754c617faa4a88a929d45bf596945183
n00nchi <alfredooviedo@Alfredos-MBP.lan>
2024-10-01 21:39:48 -0500

    Fix issue #895: Handle requests to ::1 in localhost test - 6 of 6 test pass

--- test/localhost.test.js
+++ test/localhost.test.js
@@ -6,23 +6,23 @@ const http = require('http');
 const request = require('request');
 
 test('can connect from all localhost addresses', t => {
-    const server = http.createServer(ecstatic(`${__dirname}/public/subdir`));
-    t.on('end', () => { server.close(); });
-    server.listen(0, () => {
-        const port = server.address().port;
-        const addresses = [
-            'localhost',
-            '127.0.0.1',
-            '::1',
-        ];
+  const server = http.createServer(ecstatic(`${__dirname}/public/subdir`));
+  t.on('end', () => { server.close(); });
+  server.listen(0, () => {
+    const port = server.address().port;
+    const addresses = [
+      'localhost',
+      '127.0.0.1',
+      '::1',
+    ];
 
-        t.plan(addresses.length * 2);
-        
-        for (const address of addresses) {
-            request.get(`http://${address}:${port}/index.html`, (err, res, body) => {
-                t.error(err);
-                t.equal(res.statusCode, 200);
-            });
-        }
-    });
+    t.plan(addresses.length * 2);
+
+    for (const address of addresses) {
+      request.get(`http://[${address}]:${port}/index.html`, (err, res, body) => {
+        t.error(err);
+        t.equal(res.statusCode, 200);
+      });
+    }
+  });
 });
